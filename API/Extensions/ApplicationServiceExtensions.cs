6ef3705830e8f6bd52a59b1dedd84a103b339ca3
Pedro Martinez <1999pedromartinez@gmail.com>
2022-08-02 19:48:43 -0300

    End of section 5

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    //static class significa que no necesitamos crear nuevas instancias de la misma
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            }); 
+            return services;
+        }
+    }
+}
